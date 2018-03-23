3ab63c54b4094a9a9761462ec2e0ae61548619ad
JackArt51 <34303487+JackArt51@users.noreply.github.com>
2018-03-23 20:40:34 +0100

    Responsive v0.1
    Responsive navbar
    + début login (test)

--- application/views/acceuil.php
+++ application/views/acceuil.php
@@ -1,31 +1,31 @@
+<div id="demo" class="carousel slide" data-ride="carousel">
 
+  <!-- Target des slides à afficher -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
 
-<div id='carousel-example-generic' class='carousel slide' data-ride='carousel'>
-    <!-- Target des slides à afficher -->
-    <ol class='carousel-indicators'>
-        <li data-target='#carousel-example-generic' data-slide-to='0' class='active'></li>
-        <li data-target='#carousel-example-generic' data-slide-to='1'></li>
-        <li data-target='#carousel-example-generic' data-slide-to='2'></li>
-    </ol>
-
-    <!-- Affichage des "slides". -->
-    <div class='carousel-inner'>
-        <div class='item active'>
-            <img src='<?php echo site_url(); ?>assets/img/carousel/1.jpg' width="100%" alt='' />
-        </div>
-        <div class='item'>
-            <img src='<?php echo site_url(); ?>assets/img/carousel/1.jpg' width="100%" alt='' />
-        </div>
-        <div class='item'>
-            <img src='<?php echo site_url(); ?>assets/img/carousel/1.jpg' width="100%" alt='' />
-        </div>
+  <!-- Affichage des "slides". -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo site_url(); ?>assets/img/carousel/1.jpg" alt="Los Angeles">
     </div>
+    <div class="carousel-item">
+      <img src="<?php echo site_url(); ?>assets/img/carousel/1.jpg" alt="Chicago">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo site_url(); ?>assets/img/carousel/1.jpg" alt="New York">
+    </div>
+  </div>
 
-    <!-- Bouton de controle pour faire défiler le  carousel -->
-    <a class='left carousel-control' href='#carousel-example-generic' data-slide='prev'>
-        <span class='glyphicon glyphicon-chevron-left'></span>
-    </a>
-    <a class='right carousel-control' href='#carousel-example-generic' data-slide='next'>
-        <span class='glyphicon glyphicon-chevron-right'></span>
-    </a>
+  <!-- Bouton de controle pour faire défiler le  carousel -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
 </div>
--- application/views/header.php
+++ application/views/header.php
@@ -2,48 +2,62 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <!-- <link rel="stylesheet" href="/oside/assets/css/style.css" /> -->
+    <link rel="stylesheet" href="<?php echo site_url(); ?>assets/css/style.css" />
     <link rel="stylesheet" href="<?php echo site_url(); ?>assets/css/icon.css" />
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo site_url(); ?>assets/css/bootstrap.min.css" />
+    <script src="<?php echo site_url(); ?>assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="<?php echo site_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo site_url(); ?>assets/js/popper.min.js"></script>
 
+
+    <link href="<?php echo site_url(); ?>assets/css/font.css" rel="stylesheet"><!-- Police Open Sans Regular -->
     <title>O.Side</title>
 </head>
 <body>
 
 <!-- Barre de navigation -->
-<nav class="navbar bg-dark navbar-dark navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-          <span class="sr-only">Menu navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <!-- Logo au milieu de la navbar -->
-        <a class="navbar-brand" style="transform: translateX(-50%); left: 50%; position: absolute;" href="#">
-            <img src="<?php echo site_url(); ?>assets/img/logo.jpg" style="margin-top: -10px; transform: rotate(-27deg); height: 285%;" alt="Logo O.Side">
-        </a>
-      </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-left">
-            <li class="active font"><a href="#" class="accueil"><img src="<?php echo site_url(); ?>/assets/img/png/grey/brush.png" alt="Logo O.Side">Accueil</a></li>
-            <li class="font"><a href="#"><img src="<?php echo site_url(); ?>assets/img/png/grey/seche.png" alt="Logo O.Side">Informations</a></li>
-            <li class="font"><a href="#"><img src="<?php echo site_url(); ?>assets/img/png/grey/ciseau.png" alt="Logo O.Side">Tarifs</a></li>
-            <li class="font"><a href="#"><img src="<?php echo site_url(); ?>assets/img/png/grey/tube.png" alt="Logo O.Side">La revue</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li class="font"><a href="#"><img src="<?php echo site_url(); ?>assets/img/png/grey/spray.png" alt="Logo O.Side">Prendre RDV</a></li>
-            <li class="font"><a href="#"><img src="<?php echo site_url(); ?>assets/img/png/grey/barbier.png" alt="Logo O.Side">Contacts</a></li>
-            <li class="font"><a href="<?php echo site_url('/pages/login');?>"><img src="<?php echo site_url(); ?>assets/img/png/grey/rasoir.png" alt="Logo O.Side">Connexion</a></li>
-            <li class="font"><a href="#"><img src="<?php echo site_url(); ?>assets/img/png/grey/chair.png" alt="Logo O.Side">Inscription</a></li>
-        </ul>
-      </div>
 
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto" >
+            <li class="nav-item g active font"><a href="#" class="nav-link"><img src="<?php echo site_url(); ?>/assets/img/png/grey/brush.png" alt="Logo O.Side">Accueil</a></li>
+            <li class="nav-item g font"><a href="#" class="nav-link"><img src="<?php echo site_url(); ?>assets/img/png/grey/seche.png" alt="Logo O.Side">Informations</a></li>
+            <li class="nav-item g font"><a href="#" class="nav-link"><img src="<?php echo site_url(); ?>assets/img/png/grey/ciseau.png" alt="Logo O.Side">Tarifs</a></li>
+            <li class="nav-item g font"><a href="#" class="nav-link"><img src="<?php echo site_url(); ?>assets/img/png/grey/tube.png" alt="Logo O.Side">La revue</a></li>
+        </ul>
     </div>
-
+    <div class="mx-auto order-0">
+        <a class="navbar-brand mx-auto logomid" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <a class="navbar-brand mx-auto" href="#"><img src="<?php echo site_url(); ?>assets/img/logo.jpg" style="height: 60px;" /></a>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item font"><a href="#" class="nav-link"><img src="<?php echo site_url(); ?>assets/img/png/grey/spray.png" alt="Logo O.Side">Prendre RDV</a></li>
+                <li class="nav-item font"><a href="#" class="nav-link"><img src="<?php echo site_url(); ?>assets/img/png/grey/barbier.png" alt="Logo O.Side">Contacts</a></li>
+                <li class="nav-item font"><a href="<?php echo site_url('/pages/login');?>" class="nav-link"><img src="<?php echo site_url(); ?>assets/img/png/grey/rasoir.png" alt="Logo O.Side">Connexion</a></li>
+                <li class="dropdown order-1">
+                    <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
+                    <ul class="dropdown-menu dropdown-menu-right mt-1">
+                      <li class="p-3">
+                            <form class="form" role="form">
+                                <div class="form-group">
+                                    <input id="emailInput" placeholder="Email" class="form-control form-control-sm" type="text" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input id="passwordInput" placeholder="Password" class="form-control form-control-sm" type="text" required="">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">Login</button>
+                                </div>
+                                <div class="form-group text-xs-center">
+                                    <small><a href="#">Forgot password?</a></small>
+                                </div>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+        </ul>
+    </div>
 </nav>
-
